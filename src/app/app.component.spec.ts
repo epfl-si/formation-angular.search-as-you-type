011fe67a12a8412b6d34e49e955dfa8a1f10c202
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2020-02-23 18:19:06 +0100

    [manual testing] App consists solely of an <app-search-as-you-type>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SearchAsYouTypeComponent } from './search-as-you-type/search-as-you-type.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+          AppComponent,
+          SearchAsYouTypeComponent
       ],
     }).compileComponents();
   }));
@@ -20,12 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('search-as-you-type');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('search-as-you-type app is running!');
   });
 });
